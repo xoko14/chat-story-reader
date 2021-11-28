d7e78977e90d4c19085cdef9d10af2cb19ddf54f
xoko14 <xoquinperezb@gmail.com>
2021-11-29 00:46:18 +0100

    few changes to how scripts are visualized

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,20 @@ import controller.ScriptReader
 
 fun main(args: Array<String>) {
     val script = ScriptReader.read(args[0])
+
+    println("${script.title} by ${script.author}")
+    println("Version ${script.version}")
+
     script.chats.forEach { chat ->
         println("\nPOV: ${chat.pov.name}")
         chat.messages.forEach { message ->
-            println("${message.sender.name}: ${message.message}")
+            println("${
+                if(message.sender==chat.pov){
+                    "Me"
+                } else{
+                    message.sender.name
+                }
+            }: ${message.message}")
         }
     }
 }
